44d0eae2ff432138269e13a55796fccfe97c6d5c
ewurasi nyarkoa <anewurasi@gmail.com>
2026-09-16 13:49:35 +0000

    chore: normalize line endings in environment files

--- africanhouse/src/environments/environment.prod.ts
+++ africanhouse/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://qcmjoqlggctjdyhfooer.supabase.co',
   supabaseKey: 'sb_publishable_E4BAVONMzAtaSw-WJOo4NA_dkQJQFnq',
-  paystackKey: 'pk_live_REPLACE_WITH_YOUR_LIVE_KEY'
+  paystackKey: 'pk_live_406a52420e73a06b5dad7dd65ae37007987a522c'
 };
--- africanhouse/src/environments/environment.ts
+++ africanhouse/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://qcmjoqlggctjdyhfooer.supabase.co',
   supabaseKey: 'sb_publishable_E4BAVONMzAtaSw-WJOo4NA_dkQJQFnq',
-  paystackKey: 'pk_test_51ce20db2d324ac4271b276e74986c3ba26884e0'
+  paystackKey: 'pk_live_406a52420e73a06b5dad7dd65ae37007987a522c',
 };
